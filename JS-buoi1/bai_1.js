613d277d6fcc4b3ea5042af7cbbbdcb0b50538a5
ptiendat17 <datptf8217@fullstack.edu.vn>
2025-03-10 16:16:03 +0700

    Update code with new changes

--- JS-buoi1/bai_1.js
+++ JS-buoi1/bai_1.js
@@ -1,4 +1,33 @@
 // Bài 1
+/*                                                            
+    [ Var n ]                                                 
+        │                                                     
+        ▼                                                     
+  [ Is n <= 1? ] -- Yes --> [ Return false ] --> [ End ]      
+        ▼                                                     
+        No                                                    
+        │                                                     
+        ▼                                                     
+  [ i = 2 ]                                                   
+        │                                                     
+        ▼                                                     
+  [ Is i <= √n? ] -- No --> [ Return true ] --> [ End ]       
+        ▼                                                     
+        Yes                                                   
+        │                                                     
+        ▼                                                     
+  [ Is n % i === 0? ] -- Yes --> [ Return false ] --> [ End ] 
+        ▼                                                     
+        No                                                    
+        │                                                     
+        ▼                                                     
+  [ i = i + 1 ]                                               
+        │                                                     
+        ▼                                                     
+  [ Go back to Is i <= √n? ]                                                                                             
+*/
+
+
 function isPrime(n) {
     if (n <= 1) return false; 
     for (let i = 2; i <= Math.sqrt(n); i++) {
@@ -16,7 +45,63 @@ if (isPrime(number)) {
     console.log(number + " không phải là số nguyên tố.");
 }
 
+
+
+
 //Bài 2
+/*                                                                       
+  [ Input n ]                                                              
+        │                                                                  
+        ▼                                                                  
+  [ Is n < 0? ] -- Yes --> [ Return false ] --> [ End ]                    
+        ▼                                                                  
+        No                                                                 
+        │                                                                  
+        ▼                                                                  
+  [ sqrt = Math.sqrt(n) ]                                                  
+        │                                                                  
+        ▼                                                                  
+  [ Is sqrt === Math.floor(sqrt)  ] -- Yes --> [ Return true ] --> [ End ] 
+        ▼                                                                  
+        No                                                                 
+        │                                                                  
+        ▼                                                                  
+  [ Return false ]                                                         
+        │                                                                  
+        ▼                                                                  
+  [ End ]                                                                                                                                            
+*/
+
+
+//Bài 3
+/*
+ [ Var n ]                                  
+     │                                      
+     ▼                                      
+ [ Is n <= 1 ] --> Yes --> False ]          
+     │                                      
+     ▼                                      
+ [ Var sum = 0 ]                            
+     │                                      
+     ▼                                      
+ [ For i = 1 to i <= Math.sqrt(n) ]         
+     │                                      
+     ▼                                      
+ [ n % i === 0 ] --> No --> False           
+     │                                      
+     ▼                                      
+ [ sum += i ]                               
+     │                                      
+     ▼                                      
+ [ i !== 1 && i !== n / i ] --> No --> False
+     │                                      
+     ▼                                      
+ [ sum += n / i ]                           
+     │                                      
+     ▼                                      
+   [END]                                    
+*/
+
 function isPerfectSquare(n) {
     if (n < 0) return false;
     let sqrt = Math.sqrt(n);
