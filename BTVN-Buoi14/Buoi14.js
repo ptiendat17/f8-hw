f42936c40309fcd74bb53dab8b0c4db885ddf0da
ptiendat17 <datptf8217@fullstack.edu.vn>
2025-03-19 14:50:10 +0700

    BTVN-Buoi14

--- BTVN-Buoi14/Buoi14.js
+++ BTVN-Buoi14/Buoi14.js
@@ -16,7 +16,36 @@ const companyA = [
 
 
   //1. Viết hàm findCommonNames(arr1, arr2) để tìm những người trùng tên trong cả hai danh sách.
-
+/*                                                                
+   ┌────────────────────────────────────────────────────────┐    
+   │ Let array = [],findName = []                           │    
+   │             │                                          │    
+   │             │                                          │    
+   │             ▼                                          │    
+   │    for i=0,i<arr1.length                               │    
+   │             │       │                                  │    
+   │             │       │   ┌────────────────────────────┐ │    
+   │             │       └──►│ let temp = arr1[i].name    │ │    
+   │             │           │ array[temp] = arr1[i].name │ │    
+   │             │           └────────────────────────────┘ │    
+   │             │                                          │    
+   │             │                                          │    
+   │             ▼                                          │    
+   │    for i=0,i<arr2.length                               │    
+   │             │       │                                  │    
+   │             │       │   ┌────────────────────────────┐ │    
+   │             │       └──►│ temp = arr2[i].name        │ │    
+   │             │           │ if (array[temp]) {         │ │    
+   │             │           │     findName[temp] = temp  │ │    
+   │             │           │ }                          │ │    
+   │             │           └────────────────────────────┘ │    
+   │             │                                          │    
+   │             ▼                                          │    
+   │       return findName                                  │    
+   │                                                        │    
+   │                                                        │    
+   └────────────────────────────────────────────────────────┘                                                                
+*/
   function findCommonNames(arr1, arr2) {
     let array = {}
     let findName = {}
@@ -36,3 +65,63 @@ console.log(findCommonNames(companyA, companyB))
 
 
   //2. Viết hàm findCommonAgesAndName(arr1, arr2) để tìm những người có cùng độ tuổi và tên trong cả hai danh sách.
+    /*
+      ┌─────────────────────────────────────────────────────────────────────────────────────────────────┐  
+  │ let recordsMap = {};                                                                            │  
+  │ let fullIdentity;                                                                               │  
+  │ let duplicateRecords = {};                                                                      │  
+  │       │                                                                                         │  
+  │       │                                                                                         │  
+  │       ▼                                                                                         │  
+  │ for i=0;i<array1.length                                                                         │  
+  │       │           │                                                                             │  
+  │       │           │                                                                             │  
+  │       │           │    ┌────────────────────────────────┬──────────────────────────────────────┐│  
+  │       │           └───►│ fullIdentity = arr1[i].name + "│" + arr1[i].age;                      ││  
+  │       │                │ recordsMap[fullIdentity] = { name: arr1[i].name, age: arr1[i].age };  ││  
+  │       │                └───────────────────────────────────────────────────────────────────────┘│  
+  │       │                                                                                         │  
+  │       ▼                                                                                         │  
+  │ for i=0;i<array2.length                                                                         │  
+  │       │           │                                                                             │  
+  │       │           │                                                                             │  
+  │       │           │    ┌───────────────────────────────────────────────────┐                    │  
+  │       │           └───►│ fullIdentity = arr2[i].name + " " + arr2[i].age;  │                    │  
+  │       │                │ if (recordsMap[fullIdentity]) {                   │                    │  
+  │       │                │     duplicateRecords[fullIdentity] = fullIdentity;│                    │  
+  │       │                │ }                                                 │                    │  
+  │       │                └───────────────────────────────────────────────────┘                    │  
+  │       │                                                                                         │  
+  │       ▼                                                                                         │  
+  │                                                                                                 │  
+  │ return duplicateRecords;                                                                        │  
+  └─────────────────────────────────────────────────────────────────────────────────────────────────┘  
+                                                                                                       
+    */
+
+
+  function findCommonAgesAndNames(arr1, arr2) {
+    let recordsMap = {};
+    let fullIdentity;
+    let duplicateRecords = {};
+
+    for (let i = 0; i < arr1.length; i++) {
+        fullIdentity = arr1[i].name + " " + arr1[i].age;
+        recordsMap[fullIdentity] = { name: arr1[i].name, age: arr1[i].age };
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        fullIdentity = arr2[i].name + " " + arr2[i].age;
+        if (recordsMap[fullIdentity]) {
+            duplicateRecords[fullIdentity] = fullIdentity;
+        }
+    }
+
+    return duplicateRecords;
+}
+
+console.log(findCommonAgesAndNames(companyA, companyB));
+
+
+
+
